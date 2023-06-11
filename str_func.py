7872a7a8aebca8eb20f3009fa4f8b8023ebe1c0e
Burlakov Anton <burlakovanton99@yandex.ru>
2023-06-11 18:48:48 +0300

    add func_2 in str_func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,20 @@
 def func(word):
+    """
+
+    :param word:
+    :return:
+    """
     return word.upper()
+
+
+def func_2(word):
+    """
+
+    :param word:
+    :return:
+    """
+    words_list = word.split()
+    result = []
+    for word in words_list:
+        result.append(word.capitallize())
+    return ' '.join(result)
